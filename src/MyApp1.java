232039acef0294b965364bcff7dde107f46162bd
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-04 13:44:15 +0900

    Add MyApp1.java with basic arithmetic operations

---
+++ src/MyApp1.java
@@ -0,0 +1,12 @@
+package src;
+
+public class MyApp1 {
+    public static void main(String[] args) {
+        int price = 150;
+        final double TAX = 1.1;
+
+        System.out.println(price * 120 * TAX);
+        System.out.println(price * 130 * TAX);
+        System.out.println(price * 140 * TAX);
+    }
+}
