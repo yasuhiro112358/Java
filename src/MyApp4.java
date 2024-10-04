a5c47823b3331e5144c8fd3e270d1c32c8c2596e
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-04 15:14:55 +0900

    Refactor score grading logic in MyApp4.java

--- src/MyApp4.java
+++ src/MyApp4.java
@@ -5,18 +5,20 @@ import java.util.Scanner; // input from console
 public class MyApp4 {
     public static void main(String[] args) {
         System.out.print("Please input a score: ");
-        int score = new Scanner(System.in).nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int score = scanner.nextInt();
 
-        if (score >= 90) {
-            System.out.println("S");
-        } else if (score >= 80) {
-            System.out.println("A");
-        } else if (score >= 70) {
-            System.out.println("B");
-        } else if (score >= 60) {
-            System.out.println("C");
-        } else {
-            System.out.println("D");
+            if (score >= 90) {
+                System.out.println("S");
+            } else if (score >= 80) {
+                System.out.println("A");
+            } else if (score >= 70) {
+                System.out.println("B");
+            } else if (score >= 60) {
+                System.out.println("C");
+            } else {
+                System.out.println("D");
+            }
         }
     }
 }
