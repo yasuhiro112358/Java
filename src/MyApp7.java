5ca0d74e60022a45107db32d1f00f7760678b03c
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-04 17:19:20 +0900

    Add MyApp7.java with sum calculation functionality

---
+++ src/MyApp7.java
@@ -0,0 +1,12 @@
+
+package src; // package declaration
+
+public class MyApp7 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            System.out.println(sum);
+        }
+    }
+}
