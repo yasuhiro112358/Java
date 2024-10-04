dc1280f7221742cc12678536c767a0977264f3ee
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-04 19:20:55 +0900

    Add MyApp13.java with square calculation functionality

---
+++ src/MyApp13.java
@@ -0,0 +1,21 @@
+package src; // package declaration
+
+// import java.util.Scanner; // import statement
+
+public class MyApp13 {
+    private static void showSquare(int number) {
+        double square = Math.pow(number, 2);
+        System.out.println(square);
+    }
+
+    // Method overloading
+    private static void showSquare(double number) {
+        double square = Math.pow(number, 2);
+        System.out.println(square);
+    }
+
+    public static void main(String[] args) {
+        showSquare(3);
+        showSquare(3.5);
+    }
+}
