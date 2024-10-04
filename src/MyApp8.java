a94d0a5eba73109754275f5d67f446e8dc93e919
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-04 17:23:52 +0900

    Add MyApp8.java with score printing functionality

---
+++ src/MyApp8.java
@@ -0,0 +1,11 @@
+
+package src; // package declaration
+
+public class MyApp8 {
+    public static void main(String[] args) {
+        int[] scores = { 45, 89, 67, 34, 100 };
+        for (int score : scores) {
+            System.out.println(score);
+        }
+    }
+}
