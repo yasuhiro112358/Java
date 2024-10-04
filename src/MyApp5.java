c75267c4fead2ca97cc034501f4a10628989c427
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-04 15:15:01 +0900

    Refactor traffic signal functionality in MyApp5.java

--- src/MyApp5.java
+++ src/MyApp5.java
@@ -5,12 +5,13 @@ import java.util.Scanner; // input from console
 public class MyApp5 {
     public static void main(String[] args) {
         System.out.print("Please input a signal: ");
-        String signal = new Scanner(System.in).next();
-
-        if (signal.equals("red") == true) {
-            System.out.println("Stop");
-        } else {
-            System.out.println("Not red");
+        try (Scanner scanner = new Scanner(System.in)) {
+            String signal = scanner.next();
+            if (signal.equals("red") == true) {
+                System.out.println("Stop");
+            } else {
+                System.out.println("Not red");
+            }
         }
     }
 }
