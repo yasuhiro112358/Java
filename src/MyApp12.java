57ff644530a01730879cfe8cffdc46d42ef98762
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-04 19:03:20 +0900

    Add MyApp12.java with dividend and divisor input functionality

---
+++ src/MyApp12.java
@@ -0,0 +1,35 @@
+package src; // package declaration
+
+import java.util.Scanner; // import statement
+
+public class MyApp12 {
+    private static boolean isDividable(int dividend, int divisor) {
+        if (divisor == 0) {
+            return false;
+        }
+
+        if (dividend % divisor == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int dividend;
+        int divisor;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Please input a dividend: ");
+            dividend = scanner.nextInt();
+            System.out.print("Please input a divisor: ");
+            divisor = scanner.nextInt();
+        }
+
+        if (isDividable(dividend, divisor)) {
+            System.out.println("Dividable");
+        } else {
+            System.out.println("Not dividable");
+        }
+    }
+}
