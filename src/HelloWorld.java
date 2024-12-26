e6c57c327c12100ce75d48b2c85e4d59e1302fba
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-26 19:37:30 +0900

    Update .gitignore and add comments to HelloWorld.java (from Eclipse)
    - Add Eclipse-specific files to .gitignore
    - Add build directories to .gitignore
    - Add IDE-specific files to .gitignore
    - Add OS-specific files to .gitignore
    - Add logs and temporary files to .gitignore
    - Add class comment to HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,14 @@
 package src;
 
+/**
+ * HelloWorld class
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
+
+
+
+
